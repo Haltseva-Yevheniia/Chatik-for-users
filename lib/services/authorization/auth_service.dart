59a53a50643e53fea620dcaef3278c766e09444c
Haltseva-Yevheniia <evgeniya.extremeparty@gmail.com>
2023-12-27 00:32:16 +0100

    added  in AuthService method RegistrUp and logOut

--- lib/services/authorization/auth_service.dart
+++ lib/services/authorization/auth_service.dart
@@ -1,16 +1,50 @@
-
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 
 class AuthService {
-  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  //final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
-  Future<UserCredential> signInWithEmail ({required String email, required String password}) async {
+  Future<UserCredential> signInWithEmail({
+    required String email,
+    required String password,
+    String? name,
+    Image? avatar,
+  }) async {
     try {
       UserCredential userCredential = await _firebaseAuth
           .signInWithEmailAndPassword(email: email, password: password);
+      // _firebaseFirestore.collection('users').doc(userCredential.user!.uid).set(
+      //   {
+      //     'uid': userCredential.user!.uid,
+      //     'email': email,
+      //     'name': name ?? name,
+      //     'avatar': avatar ?? avatar,
+      //   },
+      //   SetOptions(merge: true,),
+      // );
 
-    return userCredential;} on FirebaseAuthException catch (error){
+      return userCredential;
+    } on FirebaseAuthException catch (error) {
       throw Exception(error.message);
     }
+  }
+
+  Future<UserCredential> registrUp({
+    required String email,
+    required String password,
+  }) async {
+    UserCredential userCredential =
+    await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    return userCredential;
+  }
+
+  Future<void> logOut() async {
+    await _firebaseAuth.signOut();
   }
 }
