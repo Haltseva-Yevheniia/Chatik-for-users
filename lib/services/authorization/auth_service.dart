5f73656bfa504bbc511860e5b5ffcc48542b8391
Haltseva-Yevheniia <evgeniya.extremeparty@gmail.com>
2023-12-26 10:33:10 +0100

    added AuthService

---
+++ lib/services/authorization/auth_service.dart
@@ -0,0 +1,16 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<UserCredential> signInWithEmail ({required String email, required String password}) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+
+    return userCredential;} on FirebaseAuthException catch (error){
+      throw Exception(error.message);
+    }
+  }
+}
