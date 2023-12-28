5560f65e9da4a6799031f796c964e1a2c5c596ab
Inna Kotlovianova <Inna.Kotlovanova@gmail.com>
2023-12-28 03:21:22 +0100

    Add lottie

--- lib/screens/authorization/login_screen.dart
+++ lib/screens/authorization/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:chatik_for_users/services/authorization/auth_service.dart';
 import 'package:chatik_for_users/widgets/users_elevated_button.dart';
 import 'package:chatik_for_users/widgets/users_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 // import '../../widgets/users_elevated_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,10 +15,17 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+final AuthService _authService = AuthService();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  signIn() {}
+  void signIn() async {
+    try {
+      await _authService.signInWithEmail(email: emailController.text, password: passwordController.text);
+      catch (error){
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,27 +33,46 @@ class _LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Login Page'),
-        titleTextStyle: const TextStyle(fontFamily: 'Monserrat'),
+        titleTextStyle: const TextStyle(fontFamily: 'Montserrat'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          UsersTextField(
-            controller: emailController,
-            label: 'E-mail',
-            hintText: 'Enter your email',
-          ),
-          UsersTextField(
-            controller: passwordController,
-            label: 'Password',
-            hintText: 'Enter your password',
-          ),
-          UsersElevatedButton(
-            title: 'Sign In'.toUpperCase(),
-            onPressed: () => signIn(),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Lottie.asset('assets/animation/snowman.json',
+            height: 250,
+            width: 180),
+            Text(
+              'Log in'.toUpperCase(),
+              style: const TextStyle(
+                fontSize: 30,
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w800,
+                color: Colors.purple,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            UsersTextField(
+              controller: emailController,
+              label: 'E-mail',
+              hintText: 'Enter your email',
+            ),
+            const SizedBox(height: 15),
+            UsersTextField(
+              controller: passwordController,
+              label: 'Password',
+              hintText: 'Enter your password',
+            ),
+            const SizedBox(height: 30,),
+            UsersElevatedButton(
+              title: 'Sign In'.toUpperCase(),
+              onPressed: () => signIn(),
+            ),
+          ],
+        ),
       ),
     );
   }
